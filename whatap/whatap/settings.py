3687d8e97219b0ac0d365589ff1746e64cd382fd
rhkrehduq <rhkrehduq@naver.com>
2021-09-23 22:30:26 +0900

    미들웨어 명칭 수정

--- whatap/whatap/settings.py
+++ whatap/whatap/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whatap.middleware.ResponseFormattingMiddleware',
+    'whatap.middleware.ResponseMonitoringMiddleware',
 ]
 
 ROOT_URLCONF = 'whatap.urls'
